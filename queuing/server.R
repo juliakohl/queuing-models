1056cdaf2e2fe7df37670753a5f21c005082d0bd
Julia <julia.helmine.kohl@gmail.com>
2022-11-29 10:52:58 +0100

    mm1 model and bais output

--- queuing/server.R
+++ queuing/server.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(queueing)
+library(plotly)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -20,5 +21,21 @@ shinyServer(function(input, output) {
 
     # Get queue model summary
     #summary(output_mm1)
+  })
+  
+  output$dist <- renderPlot({
+    lambda = input$no_arrivals
+    mu = input$no_served
+    input_mm1 <- NewInput.MM1(lambda = lambda, mu = mu, n = 0)
+    curve(dpois(x, input_mm1$lambda),
+          from = 0, 
+          to = 20, 
+          type = "b", 
+          lwd = 2,
+          xlab = "Number of customers",
+          ylab = "Probability",
+          main = "Poisson Distribution for Arrival Process",
+          ylim = c(0, 0.25),
+          n = 21)
   })
 })
