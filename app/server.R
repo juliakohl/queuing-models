1b2746b21957c302bfa5f54e383ca1f75647a662
Julia <julia.helmine.kohl@gmail.com>
2022-12-26 18:58:21 +0100

    simulate arrivals

--- app/server.R
+++ app/server.R
@@ -3,6 +3,8 @@ library(queueing)
 library(plotly)
 library(kableExtra)
 library(readxl)
+library(dplyr)
+library(fitdistrplus)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
@@ -49,18 +51,19 @@ shinyServer(function(input, output, session) {
     
     data <<- read_excel(inFile$datapath,
                sheet = "Sheet1")
+    arrivals <<- c()
+    
+    x <<- data %>% group_by_at(input$column) %>% summarise(n = n()) %>% group_by(n) %>% summarise(count = n())
+    x <<- rbind(x, c(0, input$totalunits - sum(x$count)))
+    
+    m <<- x$n * x$count
+    lambda <<- sum(m) / input$totalunits
+    
     
     output$inputdist <- renderPlotly({
-
-      x <- data %>% group_by_at(input$column) %>% summarise(n = n()) %>% group_by(n) %>% summarise(count = n())
-      x <- rbind(x, c(0, input$totalunits - sum(x$count)))
       
       # input data
       fig <- plot_ly(x=x$n, y=x$count, type = "bar", name = 'Input data')
-      
-      m <- x$n * x$count
-      lambda <- sum(m) / input$totalunits
-      cat(lambda)
       
       # poisson
       tmp <- data.frame(y = dpois(seq(0,nrow(x)-1),lambda)*420, x = seq(0,nrow(x)-1))
@@ -70,34 +73,44 @@ shinyServer(function(input, output, session) {
       tmp <- data.frame(y = dnorm(seq(0,nrow(x)-1),lambda)*420, x = seq(0,nrow(x)-1))
       fig <- fig %>% add_trace(tmp, x =tmp$x, y=tmp$y, type = 'scatter', mode='lines', name = 'Normal')
       
+      #gamma
+      #fit <- fitdist(data[[input$column]], distr = "gamma", method = "mle")
+      #tmp <- data.frame(y = dgamma(seq(0,nrow(x)-1),fit$estimate[1],fit$estimate[2]) * 420, x = seq(0,nrow(x)-1))
+      #fig <- fig %>% add_trace(x = tmp$x, y = tmp$y, type = 'scatter', mode='lines', name = 'Gamma')
       
       fig
     })
     
-    output$dist <- renderPlotly({
-      # lambda = input$no_arrivals
-      # mu = input$no_served
-      # 
-      # data <- data.frame(y = dpois(seq(0,2*lambda),lambda), x = seq(0,2*lambda))
-      # fig <- plot_ly(data, x = ~x, y = ~y, type = 'scatter', mode = 'lines')
-      # 
-      # fig
-      
-      if(is.null(data)){
-        return(NULL)
+    if(input$arrival_bhvr == 'Poisson'){
+      arrivals <<- round(rpois(50,lambda))
+    }else{ 
+      if(input$arrival_bhvr == 'Normal'){
+        arrivals <<- round(rnorm(50,lambda))
+        arrivals[arrivals < 0] <- 0
       }
-      
-      x <- data %>% group_by_at(input$column) %>% summarise(n = n()) %>% group_by(n) %>% summarise(count = n())
-      x <- rbind(x, c(0, input$totalunits - sum(x$count)))
-      
-      m <- x$n * x$count
-      lambda <- sum(m) / input$totalunits
-      
-      tmp <- data.frame(y = dpois(seq(0,2*lambda),lambda), x = seq(0,2*lambda))
-      fig <- plot_ly(tmp, x = ~x, y = ~y, type = 'scatter', mode = 'lines')
-      
-      fig
-    })  
+    }
+    
+    output$dist <- renderPlotly({
+      plot_ly(x = seq(1,50), y = arrivals, type = 'bar')
+    })
+    
+    # output$dist <- renderPlotly({
+    #   
+    #   if(is.null(data)){
+    #     return(NULL)
+    #   }
+    #   
+    #   x <- data %>% group_by_at(input$column) %>% summarise(n = n()) %>% group_by(n) %>% summarise(count = n())
+    #   x <- rbind(x, c(0, input$totalunits - sum(x$count)))
+    #   
+    #   m <- x$n * x$count
+    #   lambda <- sum(m) / input$totalunits
+    #   
+    #   tmp <- data.frame(y = dpois(seq(0,2*lambda),lambda), x = seq(0,2*lambda))
+    #   fig <- plot_ly(tmp, x = ~x, y = ~y, type = 'scatter', mode = 'lines')
+    #   
+    #   fig
+    # })  
   })
   
   observeEvent(input$save, {
@@ -118,14 +131,17 @@ shinyServer(function(input, output, session) {
   })
   
   output$report <- renderText({
-    lambda = input$no_arrivals
-    mu = input$no_served
+    x <<- data %>% group_by_at(input$column) %>% summarise(n = n()) %>% group_by(n) %>% summarise(count = n())
+    x <<- rbind(x, c(0, input$totalunits - sum(x$count)))
+    
+    m <<- x$n * x$count
+    lambda <<- sum(m) / input$totalunits
     if(input$system == "M/M/1"){
-      input_m <- NewInput.MM1(lambda = lambda, mu = mu, n = 0)
+      input_m <- NewInput.MM1(lambda = lambda, mu = sum(m), n = 0)
     }else{
       if(input$system == "M/M/c"){
         c = input$n_servers
-        input_m <- NewInput.MMC(lambda=lambda, mu=mu, c=c, n=0, method=0)
+        input_m <- NewInput.MMC(lambda=lambda, mu=sum(m), c=c, n=0, method=0)
       }
     }
     
@@ -138,44 +154,5 @@ shinyServer(function(input, output, session) {
     y <- capture.output(Report(output_m))
     return(y)
   })
-  
-  # output$dist <- renderPlot({
-  #   lambda = input$no_arrivals
-  #   mu = input$no_served
-  #   
-  #   #View(data.frame(y = dpois(seq(0,2*lambda),lambda), x = seq(0,2*lambda)))
-  #   
-  #   if(input$system == "M/M/1"){
-  #     input_m <- NewInput.MM1(lambda = lambda, mu = mu, n = 0)
-  #     curve(dpois(x, lambda),
-  #           from = 0, 
-  #           to = 2*lambda, 
-  #           type = "b", 
-  #           lwd = 2,
-  #           xlab = "Number of customers",
-  #           ylab = "Probability",
-  #           main = "Poisson Distribution for Arrival Process",
-  #           ylim = c(0, dpois(lambda, lambda)+0.1),
-  #           n = input_m$lambda*2+1)
-  #   }else{
-  #     if(input$system == "M/M/c"){
-  #       c = input$n_servers
-  #       input_m <- NewInput.MMC(lambda=lambda, mu=mu, c=c, n=0, method=0)
-  #       #mit plotly seq(10) und dpois(seq(10), lambda)
-  #       curve(dpois(x, input_m$lambda),
-  #             from = 0, 
-  #             to = 2*lambda, 
-  #             type = "b", 
-  #             lwd = 2,
-  #             xlab = "Number of customers",
-  #             ylab = "Probability",
-  #             main = "Poisson Distribution for Arrival Process",
-  #             ylim = c(0, dpois(lambda, lambda)+0.1),
-  #             n = lambda*2+1)
-  #     }
-  #   }
-  # })
-  
-  
   
 })
